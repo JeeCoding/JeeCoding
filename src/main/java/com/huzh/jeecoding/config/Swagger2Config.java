2a04cfa9f16ce0c41c3f831a9a02e76c069732bc
RuPorcelain <251682553@qq.com>
2020-03-18 17:29:40 +0800

    JeeCoding

---
+++ src/main/java/com/huzh/jeecoding/config/Swagger2Config.java
@@ -0,0 +1,43 @@
+package com.huzh.jeecoding.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @ClassName Swagger2Config
+ * @Description TODO
+ * @Date 2020/3/18 10:44
+ * @Author huzh
+ * @Version 1.0
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket CreatRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.huzh.jeecoding.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    public ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("JeeCoding接口文档")
+                .description("JeeCoding接口文档")
+                .contact(new Contact("JeeCoding","https://github.com/JeeCoding/JeeCoding","*****@qq.com"))
+                .version("1.0.0")
+                .build();
+    }
+}
--- src/main/java/com/huzh/jeecoding/controller/UserController.java
+++ src/main/java/com/huzh/jeecoding/controller/UserController.java
@@ -2,9 +2,11 @@ package com.huzh.jeecoding.controller;
 
 import com.huzh.jeecoding.entity.User;
 import com.huzh.jeecoding.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +17,7 @@ import java.util.List;
  * @Author huzh
  * @Version 1.0
  */
+@Api(tags = "UserController", description = "用户管理")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -22,8 +25,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/")
-    public List<User> findAll() {
+    @ApiOperation("分页查询品牌列表")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public List<User> findAll(@RequestParam(value = "pageNum", defaultValue = "1L")
+                                  @ApiParam("页码") Integer pageNum,
+                              @RequestParam(value = "pageSize", defaultValue = "3L")
+                                  @ApiParam("每页数量") Integer pageSize) {
         return userService.selectAll();
     }
 }
